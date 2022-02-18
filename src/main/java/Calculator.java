22c4b2285e0381827ca4278976c744c25be001ad
Alimaa <alimaahashim@gmail.com>
2022-02-18 11:57:28 +0000

    add calcualtor and print

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator {
+
+
+    public static int multiply( int number1, int number2){
+        int result = number1 * number2;
+        return result;
+    }
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Test print");
+
+        System.out.println(Calculator.multiply(5, 6));
     }
 }
